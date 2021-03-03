b47f9ceb49a60e33c5ff7bb273a3d7a22f6806e5
Aryan-mor <K5a13wmuUUemKoz>
2021-03-03 14:04:54 +0330

    Change useOpenWithBrowserHistory.js

--- src/helper/useOpenWithBrowserHistory.js
+++ src/helper/useOpenWithBrowserHistory.js
@@ -37,5 +37,5 @@ export default function useOpenWithBrowserHistory(defaultOpen = false) {
     handleCloseClick()
   }
 
-  return [open, handleSetOpen]
+  return [open, handleSetOpen, handleOpenClick, handleCloseClick]
 }
