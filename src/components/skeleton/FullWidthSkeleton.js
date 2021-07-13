c454c6b021e8a7cf0a26fe2a0657335a10c41153
aryan_mor <aryanmoradi74@gmail.com>
2021-07-13 15:12:34 +0430

    Add borderRadius to FullWidthSkeleton.js

--- src/components/skeleton/FullWidthSkeleton.js
+++ src/components/skeleton/FullWidthSkeleton.js
@@ -1,21 +1,24 @@
 import React from 'react'
 import MaterialSkeleton from '@material-ui/lab/Skeleton'
+import { UtilsStyle } from '../../utils/Utils'
 
 
-export default function FullWidthSkeleton({...props}) {
-
-
+export default function FullWidthSkeleton({borderRadius=5,...props}) {
 
   return (
     <MaterialSkeleton
+      variant={"rect"}
       {...props}
       style={{
-        ...props.style,
+        ...props?.style,
         position: 'absolute',
         top: 0,
         right: 0,
         left: 0,
-        bottom: 0
+        bottom: 0,
+        height:'100%',
+        width:'100%',
+        ...UtilsStyle.borderRadius(borderRadius)
       }}/>
   )
 }
