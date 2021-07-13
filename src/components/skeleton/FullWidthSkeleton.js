459819440642d9b719f47d8dbbe444ecb85c1111
aryan_mor <aryanmoradi74@gmail.com>
2021-07-13 14:52:38 +0430

    Bug fixed FullWidthSkeleton.js

--- src/components/skeleton/FullWidthSkeleton.js
+++ src/components/skeleton/FullWidthSkeleton.js
@@ -11,7 +11,7 @@ export default function FullWidthSkeleton({...props}) {
       {...props}
       style={{
         ...props.style,
-        position: 'relative',
+        position: 'absolute',
         top: 0,
         right: 0,
         left: 0,
