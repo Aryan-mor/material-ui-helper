658dc1cdbeeb2164a5a4cc59191d4e99270389da
aryan_mor <aryanmoradi74@gmail.com>
2021-07-24 14:11:24 +0430

    Add useBooleanState.js

---
+++ src/helper/state/useBooleanState.js
@@ -0,0 +1,25 @@
+import React, { useCallback } from 'react'
+import useState from './useState'
+import _ from 'lodash'
+
+
+export default function useBooleanState(initialState, {
+  validator = undefined
+} = {}) {
+
+  const [val, setVal] = useState(initialState, { validator })
+
+  const handleChange = useCallback((value) => {
+    setVal(Boolean(value))
+  }, [val, setVal])
+
+  const setTrue = useCallback(() => {
+    setVal(true)
+  }, [setVal])
+
+  const setFalse = useCallback(() => {
+    setVal(false)
+  }, [setVal])
+
+  return [val, handleChange, setTrue, setFalse]
+}
